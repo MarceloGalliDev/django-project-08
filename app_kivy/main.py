84e90b161f0d62655db11a0661893e762dd577b9
Marcelo Galli <marcelolemesgalli@hotmail.com>
2024-06-03 12:22:38 -0300

    update
    update

--- app_kivy/main.py
+++ app_kivy/main.py
@@ -1,21 +1,32 @@
+# pylint: disable=all
+# importar o App, Builder (GUI)
+# # criar o nosso aplicativo
+# # criar a função build
+
 from kivy.app import App
 from kivy.lang import Builder
+import requests
 
 
-gui = Builder.load_file('home.kv')
+GUI = Builder.load_file("tela.kv")
 
 
-class MeuApp(App):
+class MeuAplicativo(App):
     def build(self):
         return GUI
 
+    def on_start(self):
+        self.root.ids["moeda1"].text = f"Dólar R${self.pegar_cotacao('USD')}"
+        self.root.ids["moeda2"].text = f"Euro R${self.pegar_cotacao('EUR')}"
+        self.root.ids["moeda3"].text = f"Bitcoin R${self.pegar_cotacao('BTC')}"
+        self.root.ids["moeda4"].text = f"Ethereum R${self.pegar_cotacao('ETH')}"
 
-if __name__ == '__main__':
-    MeuApp.run()
+    def pegar_cotacao(self, moeda):
+        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+        requisicao = requests.get(link)
+        dic_requisicao = requisicao.json()
+        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
+        return cotacao
 
 
-
-# importar App, Builder
-# criar o app
-# criar a função build
-# toda função no kivy é criado dentro de classes
+MeuAplicativo().run()
